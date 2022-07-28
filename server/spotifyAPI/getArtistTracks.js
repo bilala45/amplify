42aa170f4585b9f86e59459757aef9608d631b43
Bilal Ali <bilalal495@outlook.com>
2022-07-28 00:06:53 -0400

    Refactor getArtistTracks module as function that returns map with all tracks for specified artist

--- server/spotifyAPI/getArtistTracks.js
+++ server/spotifyAPI/getArtistTracks.js
@@ -1,56 +1,58 @@
-import albumMap from "./getArtistAlbums.js";
-import getAccessToken from "./reqAccessToken.js";
 import axios from "axios";
 import qs from "qs";
 
-// generate access token
-const accessToken = await getAccessToken();
+/**
+ * Helper method to get an album's tracks given an album id
+ */
+const reqAlbumTracks = async (accessToken, albumId) => {
+  const albumTrackURLParams = qs.stringify({
+    limit: 50,
+  });
 
-// query parameters
-const albumTrackURLParams = qs.stringify({
-  limit: 50,
-});
-
-// store artist's tracks
-const artistTracks = new Map();
-
-// iterate through values in albumMap, pull out the id and make a request using that id
-// iterate through the tracks and add each track to a hash map
-
-for (const value of albumMap.values()) {
-  // payload for get request to retrieve album tracks for each album in map
+  // payload to retrieve an album's tracks
   const payload = {
     method: "GET",
-    url: `https://api.spotify.com/v1/albums/${value.id}/tracks?${albumTrackURLParams}`,
+    url: `https://api.spotify.com/v1/albums/${albumId}/tracks?${albumTrackURLParams}`,
     headers: {
       Authorization: `Bearer ${accessToken}`,
       "Content-Type": "application/json",
     },
   };
 
-  // method to retrieve album tracks
-  const albumTracks = async () => {
-    // axios GET request
-    try {
-      // store response object and return items
-      const res = await axios(payload);
-      return res.data.items;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // retrieve album tracks
+  try {
+    // store response object and return items
+    const res = await axios(payload);
+    return res.data.items;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  const data = await albumTracks();
+/**
+ * Gets all tracks by an artist by querying tracks from artist albums
+ * key: track name, values: { track id }
 
-  // iterate through tracks in album and add to map
-  for (const track of data) {
-    if (!artistTracks.has(track.name)) {
-      artistTracks.set(track.name, {
-        duration_ms: track.duration_ms,
-        id: track.id,
-      });
+ */
+const getArtistTracks = async (accessToken, albumMap) => {
+  const tracksMap = new Map();
+
+  // iterate through values in albumMap, pull out the id and make a request using that id
+  // iterate through the tracks and add each track to a hash map
+  for (const value of albumMap.values()) {
+    const albumTracks = await reqAlbumTracks(accessToken, 5262525);
+
+    // iterate through tracks in album and add to map
+    for (const track of albumTracks) {
+      if (!tracksMap.has(track.name)) {
+        tracksMap.set(track.name, {
+          id: track.id,
+        });
+      }
     }
   }
-}
 
-export default artistTracks;
+  return tracksMap;
+};
+
+export default getArtistTracks;

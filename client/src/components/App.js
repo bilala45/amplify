202efe43d953fc7e4579c3c9622e5bb307fc7145
Bilal Ali <bilalal495@outlook.com>
2022-07-29 13:25:52 -0400

    Create Footer component for page footer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,9 @@
+import Footer from "./Footer";
+
 const App = () => (
   <div className="App">
     <h1 className="text-3xl font-bold underline">Hello world</h1>
+    <Footer />
   </div>
 );
 
---
+++ client/src/components/Footer.js
@@ -0,0 +1,6 @@
+const Footer = () => (
+  <footer>
+    A project by <a href="https://github.com/bilala45">Bilal Ali</a>
+  </footer>
+);
+export default Footer;

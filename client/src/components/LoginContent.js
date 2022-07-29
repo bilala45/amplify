b4993300774d4276d570c4e2a33af222c4ab89f3
Bilal Ali <bilalal495@outlook.com>
2022-07-29 18:04:17 -0400

    Create LoginContent component for main body of login page

---
+++ client/src/components/LoginContent.js
@@ -0,0 +1,15 @@
+const LoginContent = () => (
+  <div>
+    <div>
+      A recommendation engine that helps you explore an artist, with a personal
+      touch
+    </div>
+    <button type="button">Login with Spotify</button>
+    <div>Why do I need to do this?</div>
+    <img
+      src="https://i.picsum.photos/id/866/1920/1080.jpg?hmac=dNBuPEp10RySTqlc5EpGw7QyrFIpBd2X88r1Ixla7pw"
+      alt="Example webpage"
+    ></img>
+  </div>
+);
+export default LoginContent;

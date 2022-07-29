4eb2663a052bad228526421eb5cc2961e3f96f79
Bilal Ali <bilalal495@outlook.com>
2022-07-29 18:03:49 -0400

    Create Header component for login page

---
+++ client/src/components/Header.js
@@ -0,0 +1,8 @@
+const Header = () => (
+  // span 2 columns (this means we consider the two columns as one and our content spans across it)
+  <div>
+    <header>Spotify Project</header>
+  </div>
+);
+
+export default Header;

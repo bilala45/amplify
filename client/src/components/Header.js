382dcbd413d307273270154f9f00bb89ae685487
Bilal Ali <bilalal495@outlook.com>
2022-07-29 18:52:23 -0400

    Fix header content alignment

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,9 +6,9 @@ const Header = () => (
   // Items are vertically aligned to the center
   // margin applied to entire container
   // ! should max screen width be handled at the LoginPage component (root of this component)?
-  <div className="flex max-w-screen-sm justify-between items-center m-4">
-    <div className="text-4xl">Spotify App</div>
-    <img className="max-h-14" src={SpotifyLogo} alt="Spotify logo"></img>
+  <div className="flex justify-between items-center mx-14 my-6">
+    <div className="text-3xl">Amplify</div>
+    <img className="max-h-12" src={SpotifyLogo} alt="Spotify logo"></img>
   </div>
 );
 

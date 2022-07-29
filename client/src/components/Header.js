4751a1b217142912b1a014ff1a62640049b58baf
Bilal Ali <bilalal495@outlook.com>
2022-07-29 18:37:46 -0400

    Add Header styling and Spotify logos for header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,14 @@
+import SpotifyLogo from "./Spotify_Logo_RGB_Black.png";
+
 const Header = () => (
-  // span 2 columns (this means we consider the two columns as one and our content spans across it)
-  <div>
-    <header>Spotify Project</header>
+  // Name and logo positioned at each end of the flexbox
+  // Flexbox container has max screen width
+  // Items are vertically aligned to the center
+  // margin applied to entire container
+  // ! should max screen width be handled at the LoginPage component (root of this component)?
+  <div className="flex max-w-screen-sm justify-between items-center m-4">
+    <div className="text-4xl">Spotify App</div>
+    <img className="max-h-14" src={SpotifyLogo} alt="Spotify logo"></img>
   </div>
 );
 
--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -3,8 +3,6 @@ import LoginContent from "./LoginContent";
 import Footer from "./Footer";
 
 const LoginPage = () => (
-  // add a grid with 2 columns (rows are implicitly created)
-  // we won't actually use the two columns in the header and footer but they'll be there
   <div>
     <Header />
     <LoginContent />

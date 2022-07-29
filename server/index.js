f854a8ccf76ad5c6bf0e9b3ab89531b62b96e920
Bilal Ali <bilalal495@outlook.com>
2022-07-28 21:08:47 -0400

    Add logger middleware to server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import { port } from "./config.js";
 import express from "express";
-import userAuth from "./routes/userAuth.js";
+import morgan from "morgan";
+import userAuthRoutes from "./routes/userAuth.js";
 
 // create express app
 const app = express();
@@ -10,10 +11,13 @@ app.listen(port, () => {
   console.log(`Artist-recommender server listening on port ${port}`);
 });
 
+// logger middleware
+app.use(morgan("tiny"));
+
 // home page route
 app.get("/", (req, res) => {
   res.send("homepage route works");
 });
 
 // user authentication routes
-app.use("/login", userAuth);
+app.use("/login", userAuthRoutes);

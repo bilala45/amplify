dbbfb2dc21dc1e94cf9f4fd9cc3890c49291a683
Bilal Ali <bilalal495@outlook.com>
2022-07-29 20:39:12 -0400

    Wrap root component in div to add background styling and set screen width/height

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,9 @@
 import LoginPage from "./LoginPage";
 
-const App = () => <LoginPage />;
+const App = () => (
+  <div className="w-screen h-screen bg-gradient-to-r from-cyan-500 to-blue-500">
+    <LoginPage />
+  </div>
+);
 
 export default App;

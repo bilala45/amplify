b95699ca60854a0192256d555225d1e2e0c0e4e4
Bilal Ali <bilalal495@outlook.com>
2022-07-30 18:40:22 -0400

    Add component for artist card generated after artist search

---
+++ client/src/components/Artist.js
@@ -0,0 +1,10 @@
+import Kendrick from "../assets/KendrickArtist.jpg";
+
+const Artist = () => (
+  <div className="flex">
+    <img src={Kendrick} alt="Kendrick Lamar"></img>
+    <div>Kendrick Lamar</div>
+  </div>
+);
+
+export default Artist;

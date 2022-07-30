2df14c3d6cb989ac3fc3bca07902226608627baa
Bilal Ali <bilalal495@outlook.com>
2022-07-29 23:15:00 -0400

    Add styling to Footer component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,18 @@
 const Footer = () => (
-  // add padding to the left and set a border above
-  // span 2 columns (this means we consider the two columns as one and our content spans across it)
-  <div>
-    A project by <a href="https://github.com/bilala45">Bilal Ali</a>
-  </div>
+  // flexbox with items pushed to the left and padding on left
+  // Set height of entire footer
+  <footer className="flex items-center px-14 h-10 bg-zinc-400">
+    {/* credits */}
+    <div>
+      A project by{" "}
+      <a
+        className="underline underline-offset-2 hover:decoration-2 hover:text-white"
+        href="https://github.com/bilala45"
+      >
+        Bilal Ali
+      </a>
+    </div>
+  </footer>
 );
+
 export default Footer;

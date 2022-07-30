7ce67e00b0bdb42184e9544a43687413cbdabc0d
Bilal Ali <bilalal495@outlook.com>
2022-07-30 02:29:46 -0400

    Update Footer component with copyright notice

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,17 +1,20 @@
 const Footer = () => (
   // flexbox with items pushed to the left and padding on left
   // Set height of entire footer
-  <footer className="flex items-center px-14 h-10 bg-zinc-400">
+  <footer className="flex items-center justify-between h-12">
     {/* credits */}
     <div>
       A project by{" "}
       <a
-        className="underline underline-offset-2 hover:decoration-2 hover:text-white"
+        className="underline underline-offset-2 hover:decoration-2 hover:text-slate-100"
         href="https://github.com/bilala45"
       >
         Bilal Ali
       </a>
+      .
     </div>
+    {/* copyright */}
+    <div>Copyright © 2022, Bilal Ali. All rights reserved.</div>
   </footer>
 );
 

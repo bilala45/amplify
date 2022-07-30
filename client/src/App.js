eeddb0e7f3b96b111a3ff4c5129bcbd5725e3cc0
Bilal Ali <bilalal495@outlook.com>
2022-07-30 14:29:19 -0400

    Fix paths to root React component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import LoginPage from "./LoginPage";
+import LoginPage from "./pages/LoginPage";
+import SearchPage from "./pages/SearchPage";
 
 const App = () => (
   // set background color with div surrounding all content
   // need h-screen so that div is the height of the entire screen (not just the height of the content inside it)
   <div className="h-screen bg-gradient-to-r from-emerald-500 to-teal-500">
     <LoginPage />
+    <SearchPage />
   </div>
 );
 

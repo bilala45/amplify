12cf5201dfbb5f3476ac69c8d4bed7471fa39016
Bilal Ali <bilalal495@outlook.com>
2022-07-31 00:40:04 -0400

    Add SearchBar component to allow users to search for artists

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,5 +1,17 @@
 const SearchBar = () => (
-  <div className="pt-20 -mt-20 h-screen pb-12 -mb-12">Just a search bar!</div>
+  // encompassing content div
+  <div className="pt-20 -mt-20 h-screen pb-12 -mb-12">
+    {/* search bar encompassed in flexbox container */}
+    <div className="flex justify-center items-center h-12">
+      <form className="w-full h-full px-12">
+        <input
+          className="rounded-full border-2 border-black w-full h-full px-6"
+          type="text"
+          placeholder="Search for an artist"
+        />
+      </form>
+    </div>
+  </div>
 );
 
 export default SearchBar;

159d50616f0f2b47b52b6b1b8fa2eec8504a9991
Bilal Ali <bilalal495@outlook.com>
2022-08-02 00:37:13 -0400

    Add effect hook in SearchBar component to retrieve access tokens from url params

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -6,6 +6,16 @@ const SearchBar = () => {
   const [searchInput, setSearchInput] = useState("");
   const [searchResults, setSearchResults] = useState([]);
 
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get("access_token");
+    const refreshToken = urlParams.get("refresh_token");
+
+    console.log(accessToken);
+    console.log(refreshToken);
+  }, []);
+
   // get search results from server
   // runs after changes to searchInput state
   useEffect(() => {

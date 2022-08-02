9b92fb79f1a149657f4b9d469162ea94184a0774
Bilal Ali <bilalal495@outlook.com>
2022-08-02 18:45:48 -0400

    Make request to search submit endpoint from search results

--- client/src/components/Artist.js
+++ client/src/components/Artist.js
@@ -1,16 +1,34 @@
-import Kendrick from "../assets/KendrickArtist.jpg";
+import { getAccessToken } from "../spotify.js";
+import axios from "axios";
 
-const Artist = () => (
-  <span className="flex items-center max-w-fit rounded-xl bg-emerald-200 drop-shadow-xl shadow-black">
-    <img
-      className="rounded-l-xl max-h-16 w-auto"
-      src={Kendrick}
-      alt="Kendrick Lamar"
-    ></img>
-    <div className="text-lg font-bold tracking-wider pl-6 pr-8">
-      Kendrick Lamar
-    </div>
-  </span>
-);
+const Artist = ({ id, name, img }) => {
+  const handleArtistClick = async () => {
+    // retrieve access token from local storage
+    const accessToken = getAccessToken();
+
+    // send response to search submit endpoint to get artist discography
+    const response = await axios.get(
+      "http://localhost:3001/api/search/submit",
+      {
+        params: { accessToken, artistId: id },
+      }
+    );
+    console.log(response);
+  };
+
+  return (
+    <button
+      onClick={handleArtistClick}
+      className="h-[4.5rem] flex items-center w-full hover:bg-emerald-300 hover:rounded-md hover:cursor-pointer"
+    >
+      <img
+        className="ml-3 max-h-14 w-14 rounded-full"
+        src={img}
+        alt={name}
+      ></img>
+      <div className="text-lg font-bold tracking-wider pl-4 pr-8">{name}</div>
+    </button>
+  );
+};
 
 export default Artist;
--- server/spotifyAPI/searchAudioFeatures.js
+++ server/spotifyAPI/searchAudioFeatures.js
@@ -1,20 +1,11 @@
-import getAccessToken from "./reqAccessToken.js";
-import getArtistId from "./GetSearchResults.js";
 import getArtistAlbums from "./getArtistAlbums.js";
 import getArtistTracks from "./getArtistTracks.js";
-import getArtistAudioFeatures from "./getTracksAudioFeatures.js";
+import getAudioFeatures from "./getTracksAudioFeatures.js";
 
 /**
  * Retrieves audio features for all tracks by some artist
  */
-const getArtistAudioFeatures = async (code) => {
-  // generate access token
-  const tokenData = await getAccessToken(code);
-  const accessToken = tokenData.access_token || null;
-
-  // query artist and retrieve artist id
-  const artistId = await getArtistId(accessToken, "Dua Lipa");
-
+const getArtistAudioFeatures = async (accessToken, artistId) => {
   // get artist albums as map
   const artistAlbums = await getArtistAlbums(accessToken, artistId);
 
@@ -22,10 +13,7 @@ const getArtistAudioFeatures = async (code) => {
   const artistTracks = await getArtistTracks(accessToken, artistAlbums);
 
   // get track audio features as map
-  const trackAudioFeatures = await getArtistAudioFeatures(
-    accessToken,
-    artistTracks
-  );
+  const trackAudioFeatures = await getAudioFeatures(accessToken, artistTracks);
 
   return trackAudioFeatures;
 };

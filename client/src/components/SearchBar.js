0452963b17c1d69503ad14abbec4d9c7e432ffef
Bilal Ali <bilalal495@outlook.com>
2022-08-02 18:46:31 -0400

    Pass artist data to Artist component and update SearchBar component styles

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -9,7 +9,7 @@ const SearchBar = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [accessToken, setAccessToken] = useState(null);
 
-  // retrieve tokens from url params
+  // retrieve token from local storage
   useEffect(() => {
     setAccessToken(getAccessToken());
   }, []);
@@ -18,7 +18,7 @@ const SearchBar = () => {
   // runs after changes to searchInput state
   useEffect(() => {
     const getSearchResults = async () => {
-      if (accessToken !== null) {
+      if (accessToken !== null && searchInput.length > 0) {
         const response = await axios.get("http://localhost:3001/api/search", {
           params: { accessToken, searchInput },
         });
@@ -30,23 +30,28 @@ const SearchBar = () => {
   }, [searchInput]);
 
   return (
-    <div>
+    <div className="mt-20 mx-auto max-w-screen-md">
       <div className="flex justify-center items-center h-12">
-        <form className="w-full h-full px-12">
+        <form className="w-full h-full">
           <input
-            className="rounded-full border-2 border-black w-full h-full px-6"
+            className="rounded-md w-full h-full px-6 outline-none bg-slate-200"
             value={searchInput}
             onChange={(e) => setSearchInput(e.target.value)}
             type="text"
-            placeholder="Search for an artist"
+            placeholder="Search for an Artist"
           />
         </form>
       </div>
-      <div>
+      <div className="flex-col bg-slate-200 rounded-md z-10">
         {searchInput.length > 0 &&
           searchResults.length > 0 &&
           searchResults.map((artist) => (
-            <Artist key={artist.id} name={artist.name} img={artist.img} />
+            <Artist
+              key={artist.id}
+              id={artist.id}
+              name={artist.name}
+              img={artist.img}
+            />
           ))}
       </div>
     </div>

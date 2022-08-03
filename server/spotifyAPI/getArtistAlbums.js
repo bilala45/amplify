95b27ef179523de4caafa512770a9621ec8a6ed5
Bilal Ali <bilalal495@outlook.com>
2022-08-03 15:23:02 -0400

    Refactor script to retrieve artist albums and change return type to JSON array

--- server/spotifyAPI/getArtistAlbums.js
+++ server/spotifyAPI/getArtistAlbums.js
@@ -2,31 +2,30 @@ import qs from "qs";
 import axios from "axios";
 
 /**
- * Helper method to return artist id for top search result for queried artist
+ * Queries the Spotify API for an artist's albums
+ * https://developer.spotify.com/documentation/web-api/reference/#/operations/get-an-artists-albums
+ * @param accessToken Access token provided after auth
+ * @param artistId The artist's Spotify ID
+ * @returns [Promise]
  */
 const reqArtistAlbums = async (accessToken, artistId) => {
   // query parameters
-  const urlAlbumParams = qs.stringify({
+  const queryParams = qs.stringify({
     include_groups: "album",
     limit: 50,
-    // restricting market to CA removes some duplicates
+    // restricting market to CA removes duplicates
     market: "CA",
   });
 
-  // payload for get request to retrieve results of artist albums query
-  const payload = {
-    method: "GET",
-    url: `https://api.spotify.com/v1/artists/${artistId}/albums/?${urlAlbumParams}`,
-    headers: {
-      // access token
-      Authorization: `Bearer ${accessToken}`,
-      "Content-Type": "application/json",
-    },
-  };
-  // axios GET request
   try {
-    // store response object and return items
-    const res = await axios(payload);
+    const res = await axios({
+      method: "GET",
+      url: `https://api.spotify.com/v1/artists/${artistId}/albums/?${queryParams}`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
     return res.data.items;
   } catch (error) {
     console.log(error.response.data);
@@ -34,35 +33,30 @@ const reqArtistAlbums = async (accessToken, artistId) => {
 };
 
 /**
- * Returns map of artist albums
- * key: album name, values: { album id, number of tracks }
+ * Processes artist albums to find unique albums
+ * @param accessToken Access token provided after auth
+ * @param artistId The artist's Spotify ID
+ * @returns [array of objects]
  */
 const getArtistAlbums = async (accessToken, artistId) => {
   const albumsArr = await reqArtistAlbums(accessToken, artistId);
-  const albumMap = new Map();
+  const albumMap = {};
 
-  // save unique albums to map
-  // ! exclude deluxe albums for now (we want to include deluxe and remove original later)
-  for (const album of albumsArr) {
-    // disclude deluxe albums
-    if (album.name.toLowerCase().includes("deluxe")) {
-      continue;
-    }
-
-    // add unique albums to map
-    // if album is already in map replace with album with more tracks
+  return albumsArr.flatMap((album) => {
+    // current album is in albumMap and has more tracks than stored album
+    // OR album is not in album map
     if (
-      !albumMap.has(album.name) ||
-      albumMap.get(album.name).total_tracks < album.total_tracks
+      (albumMap[album.name] &&
+        album.total_tracks > albumMap[album.name].tracks) ||
+      !albumMap[album.name]
     ) {
-      albumMap.set(album.name, {
-        id: album.id,
-        total_tracks: album.total_tracks,
-      });
+      // add album to albumMap for tracking
+      albumMap[album.name] = { tracks: album.total_tracks };
+      // add album to array
+      return [{ name: album.name, id: album.id, tracks: album.total_tracks }];
     }
-  }
-
-  return albumMap;
+    return [];
+  });
 };
 
 export default getArtistAlbums;

35b0deccf7d4a0c69aa8538511b17d8ee46698cd
Bilal Ali <bilalal495@outlook.com>
2022-08-03 16:45:39 -0400

    Refactor script to retrieve tracks from artist's albums and change return type to JSON array

--- server/spotifyAPI/getArtistTracks.js
+++ server/spotifyAPI/getArtistTracks.js
@@ -2,27 +2,26 @@ import axios from "axios";
 import qs from "qs";
 
 /**
- * Helper method to get an album's tracks given an album id
+ * Queries the Spotify API for an album's tracks
+ * https://developer.spotify.com/documentation/web-api/reference/#/operations/get-an-albums-tracks
+ * @param accessToken Access token provided after auth
+ * @param artistId The album's Spotify ID
+ * @returns [Promise]
  */
 const reqAlbumTracks = async (accessToken, albumId) => {
-  const albumTrackURLParams = qs.stringify({
+  const queryParams = qs.stringify({
     limit: 50,
   });
 
-  // payload to retrieve an album's tracks
-  const payload = {
-    method: "GET",
-    url: `https://api.spotify.com/v1/albums/${albumId}/tracks?${albumTrackURLParams}`,
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      "Content-Type": "application/json",
-    },
-  };
-
-  // retrieve album tracks
   try {
-    // store response object and return items
-    const res = await axios(payload);
+    const res = await axios({
+      method: "GET",
+      url: `https://api.spotify.com/v1/albums/${albumId}/tracks?${queryParams}`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
     return res.data.items;
   } catch (error) {
     console.log(error.response.data);
@@ -30,28 +29,31 @@ const reqAlbumTracks = async (accessToken, albumId) => {
 };
 
 /**
- * Gets all tracks by an artist by querying tracks from artist albums
- * key: track name, values: { track id }
+ * Processes songs in artist's albums to find unique tracks
+ * @param accessToken Access token provided after auth
+ * @param artistId Array of artist's albums
+ * @returns [array of objects]
  */
-const getArtistTracks = async (accessToken, albumMap) => {
-  const tracksMap = new Map();
+const getArtistTracks = async (accessToken, albumsArr) => {
+  // set of album tracks
+  const trackSet = new Set();
+  const albumTracks = [];
 
-  // iterate through values in albumMap, pull out the id and make a request using that id
-  // iterate through the tracks and add each track to a hash map
-  for (const value of albumMap.values()) {
-    const albumTracks = await reqAlbumTracks(accessToken, value.id);
+  for (const album of albumsArr) {
+    // query Spotify for current album in array
+    const currAlbumTracks = await reqAlbumTracks(accessToken, album.id);
 
-    // iterate through tracks in album and add to map
-    for (const track of albumTracks) {
-      if (!tracksMap.has(track.name)) {
-        tracksMap.set(track.name, {
-          id: track.id,
-        });
+    // iterate through each track in album
+    for (const track of currAlbumTracks) {
+      if (!trackSet.has(track.name)) {
+        // add album to set for tracking and to array
+        trackSet.add(track.name);
+        albumTracks.push({ name: track.name, id: track.id });
       }
     }
   }
 
-  return tracksMap;
+  return albumTracks;
 };
 
 export default getArtistTracks;
--- server/spotifyAPI/searchAudioFeatures.js
+++ server/spotifyAPI/searchAudioFeatures.js
@@ -6,16 +6,22 @@ import getAudioFeatures from "./getTracksAudioFeatures.js";
  * Retrieves audio features for all tracks by some artist
  */
 const getArtistAudioFeatures = async (accessToken, artistId) => {
-  // get artist albums as map
+  // get artist albums as array
   const artistAlbums = await getArtistAlbums(accessToken, artistId);
 
-  // get artist's tracks as map
+  // get artist's tracks as array
   const artistTracks = await getArtistTracks(accessToken, artistAlbums);
+  console.log(artistTracks);
 
-  // get track audio features as map
-  const trackAudioFeatures = await getAudioFeatures(accessToken, artistTracks);
+  // // get track audio features as map
+  // const trackAudioFeatures = await getAudioFeatures(accessToken, artistTracks);
 
-  return trackAudioFeatures;
+  // return trackAudioFeatures;
 };
+
+getArtistAudioFeatures(
+  "BQBf-pAwix6i8NJ7vlbODL5m7tPuEwH7w1O9YB5e9NZGXcoHD3bC_TcvEl_rJv6fRfTSIC3tvQf-AAXwVbzXxA5d91nWGIXk5J_4uOU3asPy-DmBaNoXAi4kkUnPOikyMLczhJWIfNibvJjnoyD0q_49uVShF5mCb_wyfZOg8EuCCwM",
+  "66CXWjxzNUsdJxJ2JdwvnR"
+);
 
 export default getArtistAudioFeatures;

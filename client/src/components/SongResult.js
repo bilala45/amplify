527dae13c9119fbe7a7feebc8bbeae00ee2049ef
Bilal Ali <bilalal495@outlook.com>
2022-08-09 02:33:16 -0400

    Update SongResult component with props for song data

--- client/src/components/SongResult.js
+++ client/src/components/SongResult.js
@@ -1,25 +1,27 @@
 import AlbumCover from "../assets/TheMelodicBlue.jpg";
 
-const SongResult = () => (
-  <span className="flex items-center max-w-screen-md max-h-16 rounded-xl bg-emerald-200 mt-8 drop-shadow-xl shadow-black">
+const SongResult = ({ name, explicit, albumName, duration, img }) => (
+  <span className="flex items-center max-h-16 rounded-xl bg-emerald-200 mt-4 drop-shadow-xl shadow-black">
     <img
-      className="max-h-20 w-auto rounded-xl"
-      src={AlbumCover}
+      className="max-h-14 w-auto rounded-lg ml-1"
+      src={img}
       alt="album cover"
     ></img>
     <div className="flex w-full justify-between items-center">
       <div className="p-4">
         <div className="flex items-center">
-          <div className="text-lg font-bold tracking-wider">
-            family ties (with Kendrick Lamar)
-          </div>
-          <span className="mx-2 text-xs font-md text-slate-100 bg-neutral-700 rounded-sm px-2 py-0.5">
-            EXPLICIT
-          </span>
+          <div className="text-lg font-bold tracking-wider">{name}</div>
+          {explicit ? (
+            <span className="mx-2 text-xs font-md text-slate-100 bg-neutral-700 rounded-sm px-2 py-0.5">
+              EXPLICIT
+            </span>
+          ) : (
+            <span></span>
+          )}
         </div>
-        <div>The Melodic Blue</div>
+        <div>{albumName}</div>
       </div>
-      <div className="p-4">4:12</div>
+      <div className="p-4">{duration}</div>
     </div>
   </span>
 );

d22d5fc6cfe2e7e6c2585ce2eb5213312578bfca
Bilal Ali <bilalal495@outlook.com>
2022-08-09 21:05:05 -0400

    Create loading component and state after user has searched for artist

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -4,8 +4,10 @@ import { getAccessToken } from "../spotify.js";
 import { BsSearch } from "react-icons/bs";
 
 // import components
+import Header from "../components/Header";
 import Artist from "../components/Artist";
 import SongResult from "../components/SongResult";
+import Loading from "../components/Loading";
 
 const SearchPage = () => {
   // tracks user input in search bar
@@ -13,6 +15,7 @@ const SearchPage = () => {
   // artist results from user query
   const [searchResults, setSearchResults] = useState([]);
   const [artistTracks, setArtistTracks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [accessToken, setAccessToken] = useState(getAccessToken());
 
   // get search results from server
@@ -28,63 +31,69 @@ const SearchPage = () => {
     };
 
     getSearchResults();
-  }, [accessToken, searchInput]);
+  }, [searchInput]);
 
   return (
-    <div>
-      {accessToken !== null ? (
-        <div className="mx-auto max-w-screen-md">
-          <div className="text-2xl sm:text-3xl text-center font-semibold">
-            Your new favorite song is right around the corner.
-          </div>
+    <div className="bg-gradient-to-r from-emerald-500 to-teal-500 px-8 sm:px-14 min-h-screen">
+      <Header />
+      <div className="max-w-screen-xl mx-auto">
+        {accessToken !== null ? (
+          <div className="mx-auto max-w-screen-md">
+            <div className="text-2xl sm:text-3xl text-center font-semibold">
+              Your new favorite song is right around the corner.
+            </div>
 
-          <div className="mt-10">
-            <form className="flex justify-between h-12 items-center rounded-md bg-white shadow-2xl shadow-slate-700">
-              <div className="px-4 text-md">
-                <BsSearch />
+            <div className="mt-10">
+              <form className="flex justify-between h-12 items-center rounded-md bg-white shadow-2xl shadow-slate-700">
+                <div className="px-4 text-md">
+                  <BsSearch />
+                </div>
+                <input
+                  className="rounded-md w-full h-full pr-6 outline-none"
+                  value={searchInput}
+                  onChange={(e) => setSearchInput(e.target.value)}
+                  type="text"
+                  placeholder="Search for an Artist"
+                />
+              </form>
+
+              <div className="flex-col bg-white rounded-md absolute z-20 mx-auto w-full max-w-screen-md shadow-2xl shadow-slate-700">
+                {searchInput.length > 0 &&
+                  searchResults.length > 0 &&
+                  searchResults.map((artist) => (
+                    <Artist
+                      key={artist.id}
+                      id={artist.id}
+                      name={artist.name}
+                      img={artist.img}
+                      setSearchInput={setSearchInput}
+                      setArtistTracks={setArtistTracks}
+                      setIsLoading={setIsLoading}
+                    />
+                  ))}
               </div>
-              <input
-                className="rounded-md w-full h-full pr-6 outline-none"
-                value={searchInput}
-                onChange={(e) => setSearchInput(e.target.value)}
-                type="text"
-                placeholder="Search for an Artist"
-              />
-            </form>
+            </div>
 
-            <div className="flex-col bg-white rounded-md absolute z-20 mx-auto w-full max-w-screen-md shadow-2xl shadow-slate-700">
-              {searchInput.length > 0 &&
-                searchResults.length > 0 &&
-                searchResults.map((artist) => (
-                  <Artist
-                    key={artist.id}
-                    id={artist.id}
-                    name={artist.name}
-                    img={artist.img}
-                    setSearchInput={setSearchInput}
-                    setArtistTracks={setArtistTracks}
+            <div className="pt-10 sm:px-6 pb-24">
+              {artistTracks.length > 0 &&
+                artistTracks.map((song) => (
+                  <SongResult
+                    key={song.id}
+                    name={song.name}
+                    explicit={song.explicit}
+                    albumName={song.albumName}
+                    duration={song.duration}
+                    img={song.img}
                   />
                 ))}
             </div>
-          </div>
 
-          <div className="pt-10 px-6 pb-24">
-            {artistTracks.length > 0 &&
-              artistTracks.map((song) => (
-                <SongResult
-                  key={song.id}
-                  name={song.name}
-                  explicit={song.explicit}
-                  albumName={song.albumName}
-                  duration={song.duration}
-                  img={song.img}
-                />
-              ))}
+            {isLoading && <Loading />}
           </div>
-        </div>
-      ) : (
-        window.location.reload(false)
-      )}
+        ) : (
+          window.location.reload()
+        )}
+      </div>
     </div>
   );
 };

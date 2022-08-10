4d13820aaf03cf191550f99819f76e308cd7b729
Bilal Ali <bilalal495@outlook.com>
2022-08-09 21:06:09 -0400

    Refactor page components to include Header so that background color can be changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 // page components
 import LoginPage from "./pages/LoginPage";
@@ -8,17 +6,11 @@ import AboutPage from "./pages/AboutPage";
 import SearchPage from "./pages/SearchPage";
 
 const App = () => (
-  <div className="bg-gradient-to-r from-emerald-500 to-teal-500 px-8 sm:px-14 min-h-screen">
-    <Header />
-    <div className="max-w-screen-xl mx-auto">
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/search" element={<SearchPage />} />
-      </Routes>
-      {/* <Footer /> */}
-    </div>
-  </div>
+  <Routes>
+    <Route path="/" element={<LoginPage />} />
+    <Route path="/about" element={<AboutPage />} />
+    <Route path="/search" element={<SearchPage />} />
+  </Routes>
 );
 
 export default App;

411036aa6d407e65a5cd7c25233b84f24ff60cf8
Bilal Ali <bilalal495@outlook.com>
2022-08-12 18:05:37 -0400

    Add script to request artist metadata from Spotify API

---
+++ server/spotifyAPI/getArtistData.js
@@ -0,0 +1,30 @@
+import getArtistMetadata from "./getArtistMetadata.js";
+
+/**
+ * Gets data about an artist
+ * @param accessToken Access token provided after auth
+ * @param artistId The artist's Spotify ID
+ * @returns []
+ */
+const getArtistData = async (accessToken, artistId) => {
+  // get artist metadata
+  const artistMetadata = await getArtistMetadata(accessToken, artistId);
+
+  //   // get artist's albums
+  //   const artistAlbums = await getArtistAlbums(accessToken, artistId);
+
+  // // get artist's related artists
+  // const relatedArtists = await getArtistRelatedArtists(
+  //   accessToken,
+  //   artistAlbums
+  // );
+
+  return artistMetadata;
+};
+
+getArtistData(
+  "BQDRdD1TDaOEHhvMhgXulaLbYYrm0_ghm-izRo1yxFO_Nvq4XcgBHU3vzQ7n5npiD0OEwQYNGOJldV73jOgPtqPHsPIFdPFHfmP-uqVmSjk2cIbkFTIvoMmg2s__uJrrvrzTVRVZpi8l6BslhbKnWmv0kPK4M3gumsVy_fxhqW9HoZk",
+  "66CXWjxzNUsdJxJ2JdwvnR"
+);
+
+export default getArtistData;
---
+++ server/spotifyAPI/getArtistMetadata.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+/**
+ * Queries the Spotify API for an artist's data
+ * https://developer.spotify.com/documentation/web-api/reference/#/operations/get-an-artists-albums
+ * @param accessToken Access token provided after auth
+ * @param artistId The artist's Spotify ID
+ * @returns [Promise]
+ */
+const reqArtistMetadata = async (accessToken, artistId) => {
+  try {
+    const res = await axios({
+      method: "GET",
+      url: `https://api.spotify.com/v1/artists/${artistId}`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error.response.data);
+  }
+};
+
+const getArtistMetadata = async (accessToken, artistId) => {
+  const res = await reqArtistMetadata(accessToken, artistId);
+  return {
+    name: res.name,
+    img: res.images[0].url,
+  };
+};
+
+export default getArtistMetadata;

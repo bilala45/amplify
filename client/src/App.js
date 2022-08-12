866b4685c398985ddfbb58b03a6c149931ba12d4
Bilal Ali <bilalal495@outlook.com>
2022-08-12 00:46:25 -0400

    Remove Strict Mode from app

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,34 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 // page components
 import LoginPage from "./pages/Login/LoginPage";
 import AboutPage from "./pages/About/AboutPage";
 import SearchPage from "./pages/Search/SearchPage";
+import Protected from "./components/Protected";
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<LoginPage />} />
-    <Route path="/about" element={<AboutPage />} />
-    <Route path="/search" element={<SearchPage />} />
-  </Routes>
-);
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route
+        path="/about"
+        element={
+          <Protected>
+            <AboutPage />
+          </Protected>
+        }
+      />
+      <Route
+        path="/search"
+        element={
+          <Protected>
+            <SearchPage />
+          </Protected>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+};
 
 export default App;
